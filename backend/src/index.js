9feaeb3f868f752e9c825bd7f1cdef9106b0b4d8
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2020-01-08 00:03:54 -0700

    backend structure has been created.

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+
+async function main(){
+    //app.listen(4000, () => console.log('Server on port 4000'));
+    await app.listen(4000);
+    console.log('Server on port 4000');
+}
+
+main();
